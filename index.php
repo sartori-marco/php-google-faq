a13c680996232500d3ff3f32ba78b81a34866830
marco sartori <marcosartori98@yahoo.com>
2021-02-26 15:33:07 +0100

    header ok

--- index.php
+++ index.php
@@ -34,10 +34,28 @@ $faqs = [
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <link rel="stylesheet" href="css/style.css">
    <title>Document</title>
  </head>
  <body>
    <header>
+     <div class="header-top">
+       <img src="https://lh3.googleusercontent.com/proxy/D8FoYxD0a3zL9AJju3QSdo-nENQPocN3_1vqb1zKoVljcXOMLGKnERW7O9-rhYQhGTTiiDIDYmuhClCviy8wwDn7sDc" alt="logo">
+       <h1> <a href="#">Privacy e termini</a>  </h1>
+
+     </div>
+
+     <div class="header-bottom">
+       <ul>
+         <li> <a href="#">Introduzione</a> </li>
+         <li> <a href="#">Norme sulla privacy</a></li>
+         <li><a href="#">Termini di servizio</a></li>
+         <li><a href="#">Tecnologie</a></li>
+         <li><a href="#">Domande frequenti</a></li>
+       </ul>
+
+     </div>
+
 
    </header>
 
